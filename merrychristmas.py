f0e3dfe37cd147183a46aba44150f6320b87a91b
Heena Kwag <dotaitch@gmail.com>
2018-10-31 16:20:45 +0900

    change variable's name

--- merrychristmas.py
+++ merrychristmas.py
@@ -16,8 +16,8 @@ one = "{}1{}".format(RED, NC)
 zero = "{}0{}".format(GREEN, NC)
 
 class ChristmasTree(object):
-    def __init__(self, n=3, margin_left=10, margin_topbottom=5, time=1):
-        self.n = n;
+    def __init__(self, size=3, margin_left=10, margin_topbottom=5, time=1):
+        self.size = size;
         self.margin_left = margin_left
         self.margin_topbottom = margin_topbottom
         self.time = time
@@ -27,7 +27,7 @@ class ChristmasTree(object):
 
     def nTreeElem (self):
         result = 0
-        for i in range(self.n):
+        for i in range(self.size):
             for j in range(3+i):
                 result = result + (2*sum(range(i+1))+1+(2*j))
         return result
@@ -42,14 +42,14 @@ class ChristmasTree(object):
 
             print("\n"*self.margin_topbottom)
 
-            for i in range(self.n):
+            for i in range(self.size):
                 for j in range(3+i):
-                    spaces = " " * (sum(range(self.n+1)) + 1 - sum(range(i+1)) - j + self.margin_left)
+                    spaces = " " * (sum(range(self.size+1)) + 1 - sum(range(i+1)) - j + self.margin_left)
                     tree = ''.join(tree_elem[:2*sum(range(i+1))+1+(2*j)])
                     del tree_elem[:(2*(i+j)+1)]
                     print("{}{}".format(spaces, tree))
 
-            n_last_elems = 3 + sum(range(self.n+1)) * 2
+            n_last_elems = 3 + sum(range(self.size+1)) * 2
             half_n_last_elems = int((n_last_elems+0.5)/2)
             center = self.margin_left + half_n_last_elems
             if center < 7:
